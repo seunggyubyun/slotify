209715b9f387f7a47d4338d4942dec3c33b9f929
seunggyu <seunggyu0128@gmail.com>
2018-02-19 08:14:50 +1300

    navigation menu completed

--- index.php
+++ index.php
@@ -17,94 +17,137 @@ else{
     </head>
     <body>
 
-        <div id="nowPlayingBarContainer">
+        <div id="mainContainer">
 
-    		<div id="nowPlayingBar">
+            <div id="topContainer">
 
-    			<div id="nowPlayingLeft">
-    				<div class="content">
-    					<span class="albumLink">
-    						<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
-    					</span>
+                <div id="navBarContainer">
+                    <nav class="navBar">
 
-                        <div class="trackInfo">
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/icons/logo.png">
+                        </a>
 
-                            <span class="trackName">
-                                <span>Happy Birthday</span>
-                            </span>
+                         <div class="group">
 
-                            <span class="artistName">
-                                <span>Seunggyu</span>
-                            </span>
+                             <div class="navItem">
+                                 <a href="search.php" class="navItemLink">Search
+                                     <img src="assets/images/icons/search.png" class="icon" alt="search">
+                                 </a>
+                             </div>
 
-                        </div>
-    				</div>
-    			</div>
+                         </div>
+
+                         <div class="group">
+                             <div class="navItem">
+                                 <a href="browse.php" class="navItemLink">Browse</a>
+                             </div>
+
+                             <div class="navItem">
+                                 <a href="yourMusic.php" class="navItemLink">Your music</a>
+                             </div>
+
+                             <div class="navItem">
+                                 <a href="profile.php" class="navItemLink">Seunggyu</a>
+                             </div>
+                         </div>
+                    </nav>
+
+                </div>
+            </div>
+
+            <div id="nowPlayingBarContainer">
+
+        		<div id="nowPlayingBar">
+
+        			<div id="nowPlayingLeft">
+        				<div class="content">
+        					<span class="albumLink">
+        						<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+        					</span>
+
+                            <div class="trackInfo">
+
+                                <span class="trackName">
+                                    <span>Happy Birthday</span>
+                                </span>
+
+                                <span class="artistName">
+                                    <span>Seunggyu</span>
+                                </span>
+
+                            </div>
+        				</div>
+        			</div>
 
 
-                <div id="nowPlayingCenter">
+                    <div id="nowPlayingCenter">
 
-                    <div class="content playerControls">
+                        <div class="content playerControls">
 
-                        <div class="buttons">
+                            <div class="buttons">
 
-                            <button class="controlButton shuffle" title="Shuffle Button">
-                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                            </button>
+                                <button class="controlButton shuffle" title="Shuffle Button">
+                                    <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                                </button>
 
-                            <button class="controlButton previous" title="previous Button">
-                                <img src="assets/images/icons/previous.png" alt="Previous">
-                            </button>
+                                <button class="controlButton previous" title="previous Button">
+                                    <img src="assets/images/icons/previous.png" alt="Previous">
+                                </button>
 
-                            <button class="controlButton play" title="play Button">
-                                <img src="assets/images/icons/play.png" alt="Play">
-                            </button>
+                                <button class="controlButton play" title="play Button">
+                                    <img src="assets/images/icons/play.png" alt="Play">
+                                </button>
 
-                            <button class="controlButton pause" title="pause Button" style="display: none;">
-                                <img src="assets/images/icons/pause.png" alt="Pause">
-                            </button>
+                                <button class="controlButton pause" title="pause Button" style="display: none;">
+                                    <img src="assets/images/icons/pause.png" alt="Pause">
+                                </button>
 
 
-                            <button class="controlButton next" title="next Button">
-                                <img src="assets/images/icons/next.png" alt="Next">
-                            </button>
+                                <button class="controlButton next" title="next Button">
+                                    <img src="assets/images/icons/next.png" alt="Next">
+                                </button>
 
-                            <button class="controlButton repeat" title="repeat Button">
-                                <img src="assets/images/icons/repeat.png" alt="Repeat">
-                            </button>
+                                <button class="controlButton repeat" title="repeat Button">
+                                    <img src="assets/images/icons/repeat.png" alt="Repeat">
+                                </button>
 
-                        </div>
-
-                        <div class="playbackBar">
-                            <span class="progressTime current">0.00</span>
-
-                            <div class="progressBar">
-                                <div class="progressBarBg">
-                                    <div class="progress"></div>
-                                </div>
                             </div>
 
-                            <span class="progressTime remaining">0.00</span>
+                            <div class="playbackBar">
+                                <span class="progressTime current">0.00</span>
 
+                                <div class="progressBar">
+                                    <div class="progressBarBg">
+                                        <div class="progress"></div>
+                                    </div>
+                                </div>
+
+                                <span class="progressTime remaining">0.00</span>
+
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div id="nowPlayingRight">
-                        <div class="volumeBar">
-                            <button class="controlButton volume" title="Volume button">
-                                <img src="assets/images/icons/volume.png" alt="Volume">
-                            </button>
+                    <div id="nowPlayingRight">
+                            <div class="volumeBar">
+                                <button class="controlButton volume" title="Volume button">
+                                    <img src="assets/images/icons/volume.png" alt="Volume">
+                                </button>
 
-                            <div class="progressBar">
-                                <div class="progressBarBg">
-                                    <div class="progress"></div>
+                                <div class="progressBar">
+                                    <div class="progressBarBg">
+                                        <div class="progress"></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                </div>
+                    </div>
 
+                </div>
             </div>
+
         </div>
+
+
     </body>
 </html>
